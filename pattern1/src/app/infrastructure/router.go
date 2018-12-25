1b720050ac251e0cffe9e40b458b9978e960a35a
ytanto <y.hkh.185@gmail.com>
2018-12-26 06:46:43 +0900

    update pattern1

---
+++ pattern1/src/app/infrastructure/router.go
@@ -0,0 +1,21 @@
+package infrastructure
+
+import (
+	gin "gopkg.in/gin-gonic/gin.v1"
+
+	"app/interfaces/controllers"
+)
+
+var Router *gin.Engin
+
+func init() {
+	router := gin.Default()
+
+	userController := controllers.NewUserController(NewSqlHandler())
+
+	router.POST("/users", func(c *gin.Context) { userController.Create(c) })
+	router.GET("/users", func(c *gin.Context) { userController.Index(c) })
+	router.GET("/users/:id", func(c *gin.Context) { userController.Show(c) })
+
+	Router = router
+}
